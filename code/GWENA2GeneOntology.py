36e650ade17a0d3ffb17fb78740d13691e284a78
bigyambat <bigyambat@gmail.com>
2025-02-05 16:51:59 -0800

    More Bug Fixes

--- code/GWENA2GeneOntology.py
+++ code/GWENA2GeneOntology.py
@@ -3,14 +3,14 @@
 # Conda Environment Setup
 # conda create -n gwenav2
 # conda activate gwenav2
-# pip install pandas numpy docker
+# pip install pandas numpy docker openpyxl
 
 
 # Command Line Usage:
 # python GWENA2GeneOntology.py --gwena_enrichment_file /path/to/gwena_enrichment_file.xlsx --output_directory /path/to/output_directory --metascape_download_location /path/to/metascape_download_location
 
 # Test Run
-# /Users/bigyambat/Desktop/GWENA2GeneOntology/code/GWENA2GeneOntology.py --gwena_enrichment_file /Users/bigyambat/Desktop/GWENA2GeneOntology/test_data/Enrichment_Complete.xlsx --output_directory /Users/bigyambat/Desktop/GWENA2GeneOntology/test_data/output_directory --metascape_download_location /Users/bigyambat/Desktop/GWENA2GeneOntology/msbio_v3.5.20240901
+# /Users/bigyambat/miniforge3/envs/gwenav2/bin/python3 /Users/bigyambat/Desktop/GWENA2GeneOntology/code/GWENA2GeneOntology.py --gwena_enrichment_file /Users/bigyambat/Desktop/GWENA2GeneOntology/test_data/Enrichment_Complete.xlsx --output_directory /Users/bigyambat/Desktop/GWENA2GeneOntology/test_data/output_directory --metascape_download_location /Users/bigyambat/Desktop/GWENA2GeneOntology/msbio_v3.5.20240901
 
 import pandas as pd
 import numpy as np
@@ -19,7 +19,7 @@ import argparse
 import subprocess
 import os
 import logging
-from typing impor tList, Dict
+from typing import List, Dict
 from pathlib import Path
 
 #Logging Setup
@@ -32,7 +32,7 @@ logger = logging.getLogger(__name__)
 
 class GWENAAnalysis:
     """Class to handle GWENA enrichment analysis operations"""
-    def__init__(self, input_file: str, output_dir: str, metascape_location: str):
+    def __init__(self, input_file: str, output_dir: str, metascape_location: str):
         self.input_file = input_file
         self.output_dir = output_dir
         self.metascape_location = metascape_location
@@ -51,26 +51,28 @@ class GWENAAnalysis:
         """Create Gene List from GWENA Enrichment Data"""
 
         module_files = []
-        module_path = self.output_dir / "module_gene_list"
+        module_path = Path(self.output_dir) / "module_gene_list"
         module_path.mkdir(exist_ok=True)
 
         try:
-            for i in sample_sheet['module'].unique():
-                module = sample_sheet[sample_sheet['module'] == i]
+            for i in sample_sheet['query'].unique():
+                i = int(i)
+                module = sample_sheet[sample_sheet['query'] == i]
                 file_path = module_path / f"module_{i}_gene_list.csv"
                 module.to_csv(file_path, sep="\t", index=False)
-                module_files.append(module_file)
+                module_files.append(module)
         
         except Exception as e:
             logger.error(f"Error: {str(e)}")
             raise 
+        return module_files
 
     def run_metascape_analysis(self, module_file: Path) -> None:
         """ Run Metascape container for gene analysis"""
 
         try:
-            client - docker.from_env()
-            output_path = self.output_dir / "metascape" / module_file.stem
+            client = docker.from_env()
+            output_path = Path(self.output_dir) / "metascape" / module_file.stem
 
             command = f"{self.metascape_location}/bin/ms.sh -u -o {output_path} {module_file}"
 
@@ -91,7 +93,7 @@ class GWENAAnalysis:
 
             command = [
                 "python3",
-                str(Path.home() /"miniforge3/pkgs/go-figure-1.0.2-hdfd78af_0/python-scripts/gofigure.py",
+                str(Path.home() /"miniforge3/pkgs/go-figure-1.0.2-hdfd78af_0/python-scripts/gofigure.py"),
                 "-i", str(module_file),
                 "-o", str(output_path),
             ]
@@ -107,14 +109,18 @@ class GWENAAnalysis:
             raise
 
 def main():
+
     #Create option parser
     parser = argparse.ArgumentParser(description='GWENA2GeneOntology')
     parser.add_argument('--gwena_enrichment_file', type=str, help='Input file path')
     parser.add_argument('--output_directory', type=str, help='Output directory path')
-    parser.add_argument('--metascape_download_location_download_location', type=str, help='Metascape Docker image file location')
+    parser.add_argument('--metascape_download_location', type=str, help='Metascape Docker image file location')
     args = parser.parse_args()
 
     try:
+        #Loading in gwena enrichment file as sample sheet 
+        sample_sheet = pd.read_excel(args.gwena_enrichment_file)    
+    
         #Initialize Analysis
         analysis = GWENAAnalysis(args.gwena_enrichment_file, args.output_directory, args.metascape_download_location)
 
@@ -126,9 +132,9 @@ def main():
         
         logger.info("Analysis Completed")
         
-        except Exception as e:
-            logger.error(f"Analysis Failed: {str(e)}")
-            raise
+    except Exception as e:
+        logger.error(f"Analysis Failed: {str(e)}")
+        raise
         
 if __name__ == "__main__":
     main()
